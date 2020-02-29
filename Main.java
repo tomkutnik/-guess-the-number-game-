c7635e0a6cf93c387640edd9feac86bd46a1ced8
tomkutnik <56050303+tomkutnik@users.noreply.github.com>
2020-02-29 23:40:08 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,6 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
-
-
     public static void main(String[] args) {
         double randomNumber = Math.random() * 100;
         int randomIntNumber = (int) randomNumber;
